e0d13569227edc507deb93827403caaee1c2db06
IDCM-svg <1960803853@qq.com>
2026-04-28 19:29:39 +0800

    Site updated: 2026-04-28 19:29:27

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const VERSION = "1777375615022";const preCache = ["/HFTTC.github.io/images/taichi.png","/HFTTC.github.io/images/banner.webp","/HFTTC.github.io/css/loader.css","/HFTTC.github.io/css/style.css","/HFTTC.github.io/js/script.js"];const cacheDomain = [
+const VERSION = "1777375764114";const preCache = ["/HFTTC.github.io/images/taichi.png","/HFTTC.github.io/images/banner.webp","/HFTTC.github.io/css/loader.css","/HFTTC.github.io/css/style.css","/HFTTC.github.io/js/script.js"];const cacheDomain = [
   "fonts.googleapis.com",
   "npm.webcache.cn",
   "unpkg.com",
